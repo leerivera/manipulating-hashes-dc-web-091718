b36b39cc1f4201b1afad38487f47a7d930eb6e6e
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-05 23:15:14 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,7 @@ def second_challenge
   new_array = []
   #code your solution here!
   groceries.collect do |key, value|
-    groceries[value] = new_array
+    value << new_array
     binding.pry
    
    
